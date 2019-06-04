ae5ac2dd23a81243b2e37511e19b4170efd86930
Ilona <50212229+iisilona@users.noreply.github.com>
2019-06-04 10:35:22 -0700

    this is the big boy

--- struct.c
+++ struct.c
@@ -8,7 +8,7 @@ struct planner
   char adesc;
 }alist[500];
 
-void printas(struct planner *ptr)
+void printit(struct planner *ptr)
 {
   printf("%s", ptr->aclass);
   printf(" : %s\n", ptr->aname);
@@ -16,3 +16,37 @@ void printas(struct planner *ptr)
   printf("%s", ptr->adesc);
 }
 
+void create
+{
+ FILE *plannerstorage;
+ plannerstorage = fopen(const char *restrict __filename, const char *restrict __modes)
+ if (plannerstorage == NULL) 
+    { 
+        fprintf(stderr, "\nError opening file. Sending you back...\n"); 
+        exit (1); 
+    } 
+}
+
+void access
+{
+}
+
+int main ()
+{
+  char request;
+  char assignments = "Assignments";
+  char add = "add";
+  
+  while (1)
+  {
+   printf("Welcome to your Pi Planner. To access your assignments, type 'Assignments'. To add a new assignment, type 'Add':\n");
+     fgets(input, 50, stdin);
+     sscanf(input,"%s", request);
+      if(strcmp(request, assignments)) == 0)
+       access
+       break;
+      else if(strcmp(request, add)) == 0)
+       create
+       break;
+      else printf("That wasn't a recognizable request. Please try again...\n")
+  }
