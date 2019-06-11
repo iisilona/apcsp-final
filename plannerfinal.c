d0f052a1ef7fe36022fbea5fe50b26ffcc92e987
Ilona <50212229+iisilona@users.noreply.github.com>
2019-06-10 19:18:12 -0700

    finally!

--- plannerfinal.c
+++ plannerfinal.c
@@ -15,12 +15,6 @@ struct planner
 int main ()
 {
   char input[200];
-  char input2[200];
-  char input3[200];
-  char input4[200];
-  char input5[200];
-  char input6[200];
-  char input7[200];
   char request[200];
   char nameof[200]; //name of existing assignment//
   char name[200]; //initializes new assignment name//
@@ -29,18 +23,19 @@ int main ()
   char filename2[200];
   char buffer[BUFFER_SIZE];
   int totalRead = 0;
+  char ch;
 
   while (1)
   {
-   printf("Welcome to your Pi Planner. To access your assignments, type 'Assignments'. To add a new assignment, type 'Add':\n");
+   printf("Welcome to your Pi Planner. To access your assignments, type 'Assignments'. To add a new assignment, type 'Add'. To exit the program, type 'Exit':\n");
      fgets(input, 200, stdin);
      sscanf(input,"%s", request);
 
-      if(strcmp(request, "Add"))
+      if(strcmp(request, "Assignemnts") == 0)
       {
         printf("Ah, so you want to access an assignment? OK,  what's its name?: \n");
-         fgets(input2, 200, stdin);
-         sscanf(input2, "%s", name);
+         fgets(input, 200, stdin);
+         sscanf(input, "%s", name);
          
          strcpy(filename, name);
          strcat(filename, extent);
@@ -57,56 +52,68 @@ int main ()
 
         printf("File opened successfully. Printing information... \n\n");
          
-         while(fread(&placehldr, sizeof(placehldr), 1, file))
-         {
-           printf("Assignment Name: %s\n", placehldr.aname);
-           printf("Assignment Class: %s\n", placehldr.aclass);
-           printf("Assignment Due Date: %s\n", placehldr.adue);
-           printf("Assignment Description: %s\n", placehldr.adesc);
-         }
+        while ((ch = fgetc(file)) != EOF)
+        {
+            printf("%c", ch);
+        }
          fclose (file);
          break;
         }
         
-       else if (strcmp(request, "Assignments"))
+       else if (strcmp(request, "Add") == 0)
        {
          printf("Ah, so you want to create an assignment? OK, give the file a title: \n");
-         fgets(input3, 200, stdin);
-         sscanf(input3, "%s", nameof);
+         fgets(input, 200, stdin);
+         sscanf(input, "%s", nameof);
          struct planner plans;
+         char input[200];
 
          printf("Enter the assignment name:\n");
-         fgets(input4, 200, stdin);
-         sscanf(input4,"%[^\n]%*c", plans.aname);
+         fgets(input, 200, stdin);
+         sscanf(input,"%[^\n]%*c", plans.aname);
 
          printf("Enter the class for the assignment:\n");
-         fgets(input5, 200, stdin);
-         sscanf(input5,"%[^\n]%*c", plans.aclass);
+         fgets(input, 200, stdin);
+         sscanf(input,"%[^\n]%*c", plans.aclass);
 
          printf("Enter the due date:\n");
-         fgets(input6, 200, stdin);
-         sscanf(input6,"%[^\n]%*c", plans.adue);
+         fgets(input, 200, stdin);
+         sscanf(input,"%[^\n]%*c", plans.adue);
 
          printf("Enter a description for the assignment:\n");
-         fgets(input7, 400, stdin);
-         sscanf(input7,"%[^\n]%*c", plans.adesc);
+         fgets(input, 400, stdin);
+         sscanf(input,"%[^\n]%*c", plans.adesc);
 
          FILE * fyle;
 
          strcpy(filename, nameof);
          strcat(filename, extent);
-  
+         
          fyle = fopen(filename, "w");
-         fwrite(&plans, sizeof(plans), 1, fyle);
+        
+         if(fyle == NULL)
+          {
+            printf("Error opening file. Please try again. \n");
+            exit(EXIT_FAILURE);
+          }
+        
+         fprintf(fyle,"Assignment Name: %s \n", plans.aname); 
+         fprintf(fyle,"Assignment Class: %s \n", plans.aclass); 
+         fprintf(fyle,"Assignment Due Date: %s \n", plans.adue);
+         fprintf(fyle,"Assignment Description %s \n", plans.adesc);
 
-         if(fwrite != 0)  
          printf("Contents to file written successfully !\n"); 
-         else 
-         printf("Error writing file !\n"); 
-
+        
          fclose (fyle); 
          break;
        }
+
+        else if (strcmp(request, "Exit") == 0)
+        {
+        printf("Bye!\n");
+        exit(0);
+        }
+
  printf("That wasn't a recognizable request. Please try again...\n");
  }
 }
